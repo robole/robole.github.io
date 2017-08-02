abf60069dea36fa3387ed8ca25c4b3dade16a7cc
robert oleary <robjoeol@gmail.com>
2017-08-02 19:06:02 +0200

    layout completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,8 +9,8 @@ $(document).ready(function () {
         navBarLogo: $('.navbar-logo')
     };
 
-    // // Handling the navigation links
-    //
+    // Handling the navigation links
+
     // dom.navItem.click(function () {
     //     var scrollElement = $(this).find('.nav-link').data('link'),
     //         scrollPosition = $(scrollElement).offset().top - 100;
@@ -19,7 +19,7 @@ $(document).ready(function () {
     //         scrollTop: scrollPosition
     //     }, 500);
     // });
-    //
+
     // Displaying the logo at the navigation bar
     dom.window.scroll(function () {
       console.log("scroll");
@@ -31,10 +31,25 @@ $(document).ready(function () {
         }
     });
 
-    // // Scroll to top on the logo click
+    // Scroll to top on the logo click
     // dom.navBarLogo.click(function () {
     //     $('html, body').animate({
     //         scrollTop: 0
     //     }, 300);
     // });
+
+    $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
 });
