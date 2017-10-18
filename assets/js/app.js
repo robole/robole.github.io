b1ab4b0a25b45a6d61e7c32428e0c8d165370b1c
robert oleary <robjoeol@gmail.com>
2017-10-18 18:15:05 +0200

    change footer bg

--- assets/js/app.js
+++ assets/js/app.js
@@ -49,11 +49,4 @@ $(document).ready(function () {
     }
 });
 
-$('.flip').click(function(){
-        $(this).find('.card').addClass('flipped').mouseleave(function(){
-            $(this).removeClass('flipped');
-        });
-        return false;
-    });
-
 });
