2ea70544361e2576c81adff1592cc0049db9fd67
robert oleary <robjoeol@gmail.com>
2018-04-13 17:18:48 +0200

    updated morocco. added travel posts

--- assets/js/flickr-gallery.js
+++ assets/js/flickr-gallery.js
@@ -1,21 +1,26 @@
-var endpoint = "https://api.flickr.com/services/rest/"
-var apiKey = "4912feac8c866a2c76b84eca4bb55442";
-var photosetId = "72157692999185315";
-var extras = "url_sq,url_t,url_s,url_m,url_o";
-var method = "flickr.photosets.getPhotos";
+var flickr = (function(){
+  var endpoint = "https://api.flickr.com/services/rest/"
+  var apiKey = "4912feac8c866a2c76b84eca4bb55442";
+  var extras = "url_sq,url_t,url_s,url_m,url_o";
+  var method = "flickr.photosets.getPhotos";
 
-var request = endpoint+"?method="+method+
-            "&api_key="+apiKey+
-            "&photoset_id="+photosetId+
-            "&extras="+extras+
-            "&format=json&jsoncallback=?";
-$.getJSON(request,buildGallery);
+  var exports = {};
 
-function buildGallery(data,result){
-    if(result=="success"){
-        var photos = data.photoset.photo;
-        for(var i=0; i<photos.length; i++){
-            $('<img class="gallery_image" >').attr("src",photos[i].url_m).appendTo(".gallery");
-        }
-    }
-}
+  exports.addGallery = function (photoSetId, cssSelector){
+    console.log("css" + cssSelector);
+    var request = endpoint+"?method="+method+
+                "&api_key="+apiKey+
+                "&photoset_id="+photoSetId+
+                "&extras="+extras+
+                "&format=json&jsoncallback=?";
+
+    $.getJSON(request,function(data){
+          var photos = data.photoset.photo;
+          for(var i=0; i<photos.length; i++){
+              $('<img class="gallery_image" >').attr("src",photos[i].url_m).appendTo(cssSelector);
+      }
+    });
+  }
+
+  return exports;
+})();
